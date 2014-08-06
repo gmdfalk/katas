0d599345b704f15e0c7a5f1cab488ae4f44738c8
unknown <i13az63@ATLN71078.id.dvag.com>
2014-08-06 14:57:59 +0200

    test

--- codingbat/java/src/warmup1/Warmup1Test.java
+++ codingbat/java/src/warmup1/Warmup1Test.java
@@ -1,20 +1,9 @@
 package warmup1;
 
 import static org.junit.Assert.*;
-
-import org.junit.After;
-import org.junit.Before;
 import org.junit.Test;
 
 public class Warmup1Test {
-
-	// @Before
-	// public void setUp() throws Exception {
-	// }
-	//
-	// @After
-	// public void tearDown() throws Exception {
-	// }
 
 	@Test
 	public void testSleepIn() {
