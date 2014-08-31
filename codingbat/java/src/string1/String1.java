4ab3a2678f522eb672072dc376b360c9b664465a
Max Demian <mikar@gmx.de>
2014-08-31 11:01:58 +0200

    stringsies

--- codingbat/java/src/string1/String1.java
+++ codingbat/java/src/string1/String1.java
@@ -1,16 +1,20 @@
 package string1;
 
 public class String1 {
-	
+
 	private String1() {
 	}
 
 	public static String helloName(String str) {
 
-		return "Hello " + str;
+		return String.format("Hello %s", str);
 	}
 
 	public static String makeAbba(String s1, String s2) {
-		return s1 + s2 + s2 + s1;
+		return String.format("%s%s%s%s", s1, s2, s2, s1);
+	}
+
+	public static String makeTags(String s1, String s2) {
+		return String.format("<%s>%s</%s>", s1, s2, s1);
 	}
 }
--- codingbat/java/src/string1/String1Test.java
+++ codingbat/java/src/string1/String1Test.java
@@ -13,11 +13,18 @@ public class String1Test {
 		assertThat(String1.helloName("Alice"), is("Hello Alice"));
 		assertThat(String1.helloName("X"), is("Hello X"));
 	}
-	
+
 	@Test
 	public void testMakeAbba() {
 		assertThat(String1.makeAbba("Hi", "Bye"), is("HiByeByeHi"));
 		assertThat(String1.makeAbba("Yo", "Alice"), is("YoAliceAliceYo"));
 		assertThat(String1.makeAbba("What", "Up"), is("WhatUpUpWhat"));
+	}
+
+	@Test
+	public void testMakeTags() {
+		assertThat(String1.makeTags("i", "Yay"), is("<i>Yay</i>"));
+		assertThat(String1.makeTags("i", "Hello"), is("<i>Hello</i>"));
+		assertThat(String1.makeTags("cite", "Yay"), is("<cite>Yay</cite>"));
 	}
 }
