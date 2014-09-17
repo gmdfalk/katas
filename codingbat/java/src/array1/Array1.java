551a3f8f17ff23072d5b65e6937d9f6199bc7fee
Max Demian <mikar@gmx.de>
2014-09-17 14:37:07 +0200

    makePi

--- codingbat/java/src/array1/Array1.java
+++ codingbat/java/src/array1/Array1.java
@@ -1,11 +1,17 @@
 package array1;
 
-import sun.security.util.Length;
-
 public class Array1 {
 
 	public static Boolean firstLast6(int[] is) {
 		return is[0] == 6 || is[is.length-1] == 6;
+	}
+
+	public static Boolean sameFirstLast(int[] is) {
+		return is[0] == is[is.length-1];
+	}
+
+	public static int[] makePi() {
+		return new int[] {3,1,4};
 	}
 
 }
--- codingbat/java/src/array1/Array1Test.java
+++ codingbat/java/src/array1/Array1Test.java
@@ -8,10 +8,22 @@ import org.junit.Test;
 public class Array1Test {
 
 	@Test
-	public void testStringTimes() {
-		assertThat(Array1.firstLast6(new int[] {1, 2, 6}), is(true));
-		assertThat(Array1.firstLast6(new int[]{6, 1, 2, 3}), is(true));
-		assertThat(Array1.firstLast6(new int[] {13, 6, 1, 2, 3}), is(false));
+	public void testFirstLast6() {
+		assertThat(Array1.firstLast6(new int[] { 1, 2, 6 }), is(true));
+		assertThat(Array1.firstLast6(new int[] { 6, 1, 2, 3 }), is(true));
+		assertThat(Array1.firstLast6(new int[] { 13, 6, 1, 2, 3 }), is(false));
+	}
+
+	@Test
+	public void testSameFirstLast() {
+		assertThat(Array1.sameFirstLast(new int[] { 1, 2, 3 }), is(false));
+		assertThat(Array1.sameFirstLast(new int[] { 1, 2, 3, 1 }), is(true));
+		assertThat(Array1.sameFirstLast(new int[] { 1, 2, 1 }), is(true));
+	}
+
+	@Test
+	public void testMakePi() {
+		assertThat(Array1.makePi(), is(new int[] { 3, 1, 4 }));
 	}
 
 }
