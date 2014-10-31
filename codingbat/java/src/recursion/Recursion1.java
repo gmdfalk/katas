6451c2d547f5fe736231ec6f00e01be86de797ce
Max Demian <mikar@gmx.de>
2014-10-31 13:42:15 +0100

    triangle

--- codingbat/java/src/recursion/Recursion1.java
+++ codingbat/java/src/recursion/Recursion1.java
@@ -2,15 +2,15 @@ package recursion;
 
 public class Recursion1 {
 
-	public static Integer factorial( int n ) {
+	public static int factorial( int n ) {
 		return n < 2 ? n : n * factorial( n - 1 );
 	}
 
-	public static Integer bunnyEars( int n ) {
+	public static int bunnyEars( int n ) {
 		return n < 1 ? 0 : 2 + bunnyEars( n - 1 );
 	}
 
-	public static Integer fibonacciLoop( int n ) {
+	public static int fibonacciLoop( int n ) {
 		int a = 0, b = 1, temp;
 		for ( int i = 1; i < n; i++ ) {
 			temp = b;
@@ -20,7 +20,7 @@ public class Recursion1 {
 		return n < 2 ? n : b;
 	}
 
-	public static Integer fibonacci( int n ) {
+	public static int fibonacci( int n ) {
 		int a = 0, b = 1, temp;
 		for ( int i = 1; i < n; i++ ) {
 			temp = b;
@@ -30,16 +30,22 @@ public class Recursion1 {
 		return n < 2 ? n : fibonacci( n - 1 ) + fibonacci( n - 2 );
 	}
 
-	public static Integer bunnyEars2( int n ) {
-		return n < 1 ? 0 : individualEars( n ) + bunnyEars2( n - 1 );
+	public static int bunnyEars2( int n ) {
+		if ( n < 1 )
+			return 0;
+		return determineEars( n ) + bunnyEars2( n - 1 );
 	}
 
-	private static int individualEars( int n ) {
+	private static int determineEars( int n ) {
 		return isEven( n ) ? 3 : 2;
 	}
 
 	private static boolean isEven( int n ) {
 		return n % 2 == 0;
+	}
+
+	public static int triangle( int n ) {
+		return n < 2 ? n : n + triangle( n-1 );
 	}
 
 }
--- codingbat/java/src/recursion/Recursion1Test.java
+++ codingbat/java/src/recursion/Recursion1Test.java
@@ -9,43 +9,52 @@ public class Recursion1Test {
 
 	@Test
 	public void testCigarParty() {
-		assertThat(Recursion1.factorial(1), is(1));
-		assertThat(Recursion1.factorial(2), is(2));
-		assertThat(Recursion1.factorial(3), is(6));
-		assertThat(Recursion1.factorial(4), is(24));
-		assertThat(Recursion1.factorial(5), is(120));
+		assertThat( Recursion1.factorial( 1 ), is( 1 ) );
+		assertThat( Recursion1.factorial( 2 ), is( 2 ) );
+		assertThat( Recursion1.factorial( 3 ), is( 6 ) );
+		assertThat( Recursion1.factorial( 4 ), is( 24 ) );
+		assertThat( Recursion1.factorial( 5 ), is( 120 ) );
 	}
 
 	@Test
 	public void testBunnyEars() {
-		assertThat(Recursion1.bunnyEars(0), is(0));
-		assertThat(Recursion1.bunnyEars(1), is(2));
-		assertThat(Recursion1.bunnyEars(2), is(4));
-		assertThat(Recursion1.bunnyEars(3), is(6));
-		assertThat(Recursion1.bunnyEars(4), is(8));
-		assertThat(Recursion1.bunnyEars(5), is(10));
-		assertThat(Recursion1.bunnyEars(6), is(12));
-	}
-	
-	@Test
-	public void testFibonacci() {
-		assertThat(Recursion1.fibonacci(0), is(0));
-		assertThat(Recursion1.fibonacci(1), is(1));
-		assertThat(Recursion1.fibonacci(2), is(1));
-		assertThat(Recursion1.fibonacci(3), is(2));
-		assertThat(Recursion1.fibonacci(4), is(3));
-		assertThat(Recursion1.fibonacci(5), is(5));
+		assertThat( Recursion1.bunnyEars( 0 ), is( 0 ) );
+		assertThat( Recursion1.bunnyEars( 1 ), is( 2 ) );
+		assertThat( Recursion1.bunnyEars( 2 ), is( 4 ) );
+		assertThat( Recursion1.bunnyEars( 3 ), is( 6 ) );
+		assertThat( Recursion1.bunnyEars( 4 ), is( 8 ) );
+		assertThat( Recursion1.bunnyEars( 5 ), is( 10 ) );
+		assertThat( Recursion1.bunnyEars( 6 ), is( 12 ) );
 	}
 
-	
+	@Test
+	public void testFibonacci() {
+		assertThat( Recursion1.fibonacci( 0 ), is( 0 ) );
+		assertThat( Recursion1.fibonacci( 1 ), is( 1 ) );
+		assertThat( Recursion1.fibonacci( 2 ), is( 1 ) );
+		assertThat( Recursion1.fibonacci( 3 ), is( 2 ) );
+		assertThat( Recursion1.fibonacci( 4 ), is( 3 ) );
+		assertThat( Recursion1.fibonacci( 5 ), is( 5 ) );
+	}
+
 	@Test
 	public void testBunnyEars2() {
-		assertThat(Recursion1.bunnyEars2(0), is(0));
-		assertThat(Recursion1.bunnyEars2(1), is(2));
-		assertThat(Recursion1.bunnyEars2(2), is(5));
-		assertThat(Recursion1.bunnyEars2(3), is(7));
-		assertThat(Recursion1.bunnyEars2(4), is(10));
-		assertThat(Recursion1.bunnyEars2(5), is(12));
-		assertThat(Recursion1.bunnyEars2(6), is(15));
+		assertThat( Recursion1.bunnyEars2( 0 ), is( 0 ) );
+		assertThat( Recursion1.bunnyEars2( 1 ), is( 2 ) );
+		assertThat( Recursion1.bunnyEars2( 2 ), is( 5 ) );
+		assertThat( Recursion1.bunnyEars2( 3 ), is( 7 ) );
+		assertThat( Recursion1.bunnyEars2( 4 ), is( 10 ) );
+		assertThat( Recursion1.bunnyEars2( 5 ), is( 12 ) );
+		assertThat( Recursion1.bunnyEars2( 6 ), is( 15 ) );
+	}
+
+	@Test
+	public void testTriangle() {
+		assertThat( Recursion1.triangle( 0 ), is( 0 ) );
+		assertThat( Recursion1.triangle( 1 ), is( 1 ) );
+		assertThat( Recursion1.triangle( 2 ), is( 3 ) );
+		assertThat( Recursion1.triangle( 3 ), is( 6 ) );
+		assertThat( Recursion1.triangle( 4 ), is( 10 ) );
+		assertThat( Recursion1.triangle( 5 ), is( 15 ) );
 	}
 }
