c732d68cbaa57bc096ff46c9a23aaa8b4eef01d9
Max Demian <mikar@gmx.de>
2014-11-21 09:38:02 +0100

    ap1.scores100

--- codingbat/java/src/ap/AP1.java
+++ codingbat/java/src/ap/AP1.java
@@ -11,4 +11,14 @@ public class AP1 {
 		return false;
 	}
 
+	public static boolean scores100( final int[] is ) {
+		for ( int i = 0; i < is.length - 1; i++ ) {
+			if ( is[i] == 100 && is[i + 1] == 100 ) {
+				return true;
+			}
+
+		}
+		return false;
+	}
+
 }
--- codingbat/java/src/ap/AP1Test.java
+++ codingbat/java/src/ap/AP1Test.java
@@ -13,4 +13,11 @@ public class AP1Test {
 		assertThat( AP1.scoresIncreasing( new int[] { 1, 3, 2 } ), is( true ) );
 		assertThat( AP1.scoresIncreasing( new int[] { 1, 1, 4 } ), is( true ) );
 	}
+
+	@Test
+	public void testScores100() {
+		assertThat( AP1.scores100( new int[] { 1, 100, 100 } ), is( true ) );
+		assertThat( AP1.scores100( new int[] { 1, 100, 99, 100 } ), is( false ) );
+		assertThat( AP1.scores100( new int[] { 100, 1, 100, 100 } ), is( true ) );
+	}
 }
