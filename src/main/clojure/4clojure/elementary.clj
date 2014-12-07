250cc6716ed52ac3523aceeff55a2b096c7325da
Max Demian <mikar@gmx.de>
2014-12-07 20:01:24 +0100

    Trying to integrate clojure into a maven project. FML

---
+++ src/main/clojure/4clojure/elementary.clj
@@ -0,0 +1,6 @@
+(ns 4clojure.elementary)
+
+(defn foo
+  "I don't do a whole lot."
+  [x]
+  (println x "Hello, World!"))
---
+++ src/test/clojure/4clojure/elementary_test.clj
@@ -0,0 +1,7 @@
+(ns 4clojure.elementary_test)
+  (:require [clojure.test :refer :all]
+            [4clojure.elementary :refer :all]))
+
+(deftest a-test
+  (testing "FIXME, I fail."
+    (is (= 0 1))))
