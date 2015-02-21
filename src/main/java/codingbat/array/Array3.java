4bc8d868a49826ce368392c0ea50eace5621fe29
Max Demian <mikar@gmx.de>
2015-02-21 11:05:57 +0100

    array3.maxSpan

--- src/main/java/codingbat/array/Array3.java
+++ src/main/java/codingbat/array/Array3.java
@@ -2,8 +2,16 @@ package codingbat.array;
 
 public class Array3 {
 
-	public static int maxSpan( final int[] is ) {
-		return 0;
+	public static int maxSpan( final int[] nums ) {
+		int span = 0;
+		int len = nums.length < 2 ? 0 : nums.length-1;
+		for (int i=0; i < nums.length; i++) {
+			for (int j=len; j >= 0; j--) {
+				if (nums[i] == nums[j]) {
+					span = Math.max(span, (j-i)+1);
+				}
+			}
+		}
+		return span;
 	}
-
 }
