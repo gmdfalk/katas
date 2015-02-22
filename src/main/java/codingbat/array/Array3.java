d09923d425a40eab05a9e376a336664f8fdfa824
Max Demian <mikar@gmx.de>
2015-02-22 10:49:44 +0100

    array3.fix34
    arrays are the wurst

--- src/main/java/codingbat/array/Array3.java
+++ src/main/java/codingbat/array/Array3.java
@@ -4,14 +4,40 @@ public class Array3 {
 
 	public static int maxSpan( final int[] nums ) {
 		int span = 0;
-		int len = nums.length < 2 ? 0 : nums.length-1;
-		for (int i=0; i < nums.length; i++) {
-			for (int j=len; j >= 0; j--) {
-				if (nums[i] == nums[j]) {
-					span = Math.max(span, (j-i)+1);
+		final int len = nums.length < 2 ? 0 : nums.length - 1;
+		for ( int i = 0; i < nums.length; i++ ) {
+			for ( int j = len; j >= 0; j-- ) {
+				if ( nums[i] == nums[j] ) {
+					span = Math.max( span, ( j - i ) + 1 );
 				}
 			}
 		}
 		return span;
+	}
+
+	public static int[] fix34( final int[] is ) {
+		int threeStart = 0, fourStart = 0;
+		int indexOfThree, indexOfFour;
+		int swap;
+		final int[] result = new int[is.length];
+		System.arraycopy( is, 0, result, 0, is.length );
+		while ( ( indexOfThree = findIndexOf( 3, is, threeStart ) ) != -1 ) {
+			indexOfFour = findIndexOf( 4, is, fourStart );
+			swap = is[indexOfThree + 1];
+			result[indexOfThree + 1] = is[indexOfFour];
+			result[indexOfFour] = swap;
+			threeStart = indexOfThree + 1;
+			fourStart = indexOfFour + 1;
+		}
+		return result;
+	}
+
+	private static int findIndexOf( final int n, final int[] is, final int start ) {
+		for ( int i = start; i < is.length; i++ ) {
+			if ( is[i] == n ) {
+				return i;
+			}
+		}
+		return -1;
 	}
 }
--- src/test/java/codingbat/array/Array3Test.java
+++ src/test/java/codingbat/array/Array3Test.java
@@ -14,4 +14,11 @@ public class Array3Test {
 		assertThat( Array3.maxSpan( new int[] { 1, 4, 2, 1, 4, 4, 4 } ), is( 6 ) );
 		assertThat( Array3.maxSpan( new int[] { 3 } ), is( 1 ) );
 	}
+
+	@Test
+	public void testFix34() {
+		assertThat( Array3.fix34( new int[] { 1, 3, 1, 4 } ), is( new int[] { 1, 3, 4, 1 } ) );
+		assertThat( Array3.fix34( new int[] { 1, 3, 1, 4, 4, 3, 1 } ), is( new int[] { 1, 3, 4, 1, 1, 3, 4 } ) );
+		assertThat( Array3.fix34( new int[] { 3, 2, 2, 4 } ), is( new int[] { 3, 4, 2, 2 } ) );
+	}
 }
