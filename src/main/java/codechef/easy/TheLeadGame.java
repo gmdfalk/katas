1dffb819681878ecddd8a877361c17021b42e318
Max Demian <mikar@gmx.de>
2015-03-22 09:38:13 +0100

    codechef.easy.TheLeadGame

--- src/main/java/codechef/easy/TheLeadGame.java
+++ src/main/java/codechef/easy/TheLeadGame.java
@@ -3,7 +3,26 @@ package codechef.easy;
 public class TheLeadGame {
 
 	public static int[] main(int[] is) {
-		return null;
+
+		// 140, 82, 89, 134, 90, 110, 112, 106, 88, 90
+		int player1Score = 0, player2Score = 0;
+		int maxLead = -1;
+		int leader = -1;
+		int lead = -1;
+		for (int i = 0; i < is.length-1; i++) {
+			player1Score += is[i];
+			player2Score += is[i + 1];
+			lead = Math.abs(player1Score - player2Score);
+			if (lead > maxLead) {
+				if (player1Score > player2Score) {
+					leader = 1;
+				} else {
+					leader = 2;
+				}
+				maxLead = lead;
+			}
+		}
+		return new int[] {leader, maxLead};
 	}
 
 }
