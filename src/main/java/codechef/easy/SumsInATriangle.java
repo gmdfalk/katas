781bbf3aabd5e11f46b1828ddcecd6df6742472f
Max Demian <mikar@gmx.de>
2015-03-26 19:45:13 +0100

    codechef.easy.SumsInATriangle

--- src/main/java/codechef/easy/SumsInATriangle.java
+++ src/main/java/codechef/easy/SumsInATriangle.java
@@ -2,9 +2,13 @@ package codechef.easy;
 
 public class SumsInATriangle {
 
-	public static Object main(int[] a, int[] b, int[] c, int[] d) {
-		// TODO Auto-generated method stub
-		return null;
+	public static int main(int[][] data) {
+
+		for (int r = data.length - 1; r > 0; r--)
+			for (int c = 0; c < data[r].length - 1; c++)
+				data[r - 1][c] += Math.max(data[r][c], data[r][c + 1]);
+
+		return data[0][0];
 	}
 
 }
