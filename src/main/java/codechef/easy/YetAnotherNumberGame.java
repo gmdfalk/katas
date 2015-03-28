919346c45c50ad0e9ca488b7742978294c1c17bc
Max Demian <mikar@gmx.de>
2015-03-28 18:13:22 +0100

    codechef.easy.YetAnotherNumberGame fix

--- src/main/java/codechef/easy/YetAnotherNumberGame.java
+++ src/main/java/codechef/easy/YetAnotherNumberGame.java
@@ -3,47 +3,14 @@ package codechef.easy;
 public class YetAnotherNumberGame {
 
 	public static String main(int n) {
-		Player player = new Player("ALICE");
-
-		while (n > 0) {
-			n -= findHighestDivisor(n);
-			player.swap();
-		}
-
-		return player.getName();
-	}
-
-	private static int findHighestDivisor(int n) {
-		int divisor = 1;
-		
-		for (int i=n-1; i > 0; i--) {
-			if (n % i == 0) {
-				divisor = i;
-				break;
-			}
-		}
-		
-		return divisor;
-	}
-
-}
-
-class Player {
-	private String name;
-
-	public Player(String name) {
-		this.name = name;
-	}
-
-	public String getName() {
-		return name;
-	}
-
-	public void swap() {
-		if (name.equals("ALICE")) {
-			name = "BOB";
+		if (isEven(n)) {
+			return "ALICE";
 		} else {
-			name = "ALICE";
+			return "BOB";
 		}
+	}
+	
+	private static boolean isEven(int n) {
+		return (n & 1) == 0;
 	}
 }
