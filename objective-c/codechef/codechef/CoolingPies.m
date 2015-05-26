6cc87578769500a4aec4b890754c4cf18b9853cf
Max Demian <mikar@gmx.de>
2015-05-26 19:37:16 +0200

    CoolingPies

--- objective-c/codechef/codechef/CoolingPies.m
+++ objective-c/codechef/codechef/CoolingPies.m
@@ -11,7 +11,23 @@
 @implementation CoolingPies
 
 + (NSInteger)main:(NSInteger)pies withWeights:(NSArray *)weights andMaxWeights:(NSArray *)maxWeights {
-    return 0;
+    NSSortDescriptor *sortDescending = [NSSortDescriptor sortDescriptorWithKey:@"self" ascending:NO];
+    NSMutableArray *sortedWeights = [NSMutableArray arrayWithArray:weights];
+    NSMutableArray *sortedMaxWeights = [NSMutableArray arrayWithArray:maxWeights];
+    [sortedWeights sortUsingDescriptors:[NSArray arrayWithObject:sortDescending]];
+    [sortedMaxWeights sortUsingDescriptors:[NSArray arrayWithObject:sortDescending]];
+
+//     Violating all rules of flow control and immutability ¯\_(ツ)_/¯
+    for (int i = 0; i < sortedWeights.count; i++) {
+        for (int j = 0; j < sortedMaxWeights.count; j++) {
+            if ([sortedWeights[i] intValue] <= [sortedMaxWeights[j] intValue]) {
+                [sortedWeights removeObjectAtIndex:i--];
+                [sortedMaxWeights removeObjectAtIndex:j];
+                break;
+            }
+        }
+    }
+    return pies - [sortedWeights count];
 }
 
 @end
