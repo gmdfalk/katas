71ef554efe951a21b9cab75ccdf3c4b9ac9857a0
Max Demian <mikar@gmx.de>
2015-05-31 13:25:31 +0200

    TransformTheExpression

--- swift/CodeChef/CodeChef/TransformTheExpression.swift
+++ swift/CodeChef/CodeChef/TransformTheExpression.swift
@@ -9,9 +9,45 @@
 import Foundation
 
 
+extension Array {
+    
+    mutating func pop() -> T {
+        return self.removeLast()
+    }
+    
+    mutating func push(item: T) {
+        self.append(item)
+    }
+    
+}
 
 class TransformTheExpression {
-    class func main(str : String) -> String {
-       return ""
+    
+    static let operators = [")", "(", "^", "*", "/", "-", "+"]
+    
+    class func main(infix : String) -> String {
+        var stack = [String]()
+        var postfix = ""
+        
+        for char in infix {
+            var str = String(char)
+            if contains(operators, str) {
+                if (str == operators[0]) {
+                    do {
+                        var topItem = stack.pop()
+                        if (topItem != operators[1]) {
+                            postfix += topItem
+                        }
+                    } while stack.last != operators[1]
+                } else {
+                    stack.push(str)
+                }
+            } else {
+                postfix += str
+            }
+        }
+        
+        return postfix
     }
+    
 }
