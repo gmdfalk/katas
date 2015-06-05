d77433220060afb2b00fc15b551143928d622136
Max Demian <mikar@gmx.de>
2015-06-05 18:24:33 +0200

    CuttingRecipes

--- swift/CodeChef/CodeChef/CuttingRecipes.swift
+++ swift/CodeChef/CodeChef/CuttingRecipes.swift
@@ -13,9 +13,19 @@ class CuttingRecipes {
     
     
     class func main(ingredientCount : Int, ingredients : [Int]) -> [Int] {
-        var reducedIngredients = [Int]()
-        
-        return reducedIngredients
+        var divisor = 1
+       
+        for i in 0..<ingredientCount {
+            for j in 0..<ingredientCount {
+                if (ingredients[j] % ingredients[i] > 0) {
+                    break
+                } else if (j == ingredientCount-1) {
+                    divisor = ingredients[i]
+                }
+            }
+        }
+ 
+        return ingredients.map({$0/divisor})
     }
     
 }
