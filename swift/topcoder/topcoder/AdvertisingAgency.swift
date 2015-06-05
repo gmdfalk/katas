1d166e380932bd56164f8b07af0c3c38c6f49b7a
Max Demian <mikar@gmx.de>
2015-06-05 19:03:01 +0200

    Set up topcoder project for AdvertisingAgency Tests

---
+++ swift/topcoder/topcoder/AdvertisingAgency.swift
@@ -0,0 +1,18 @@
+//
+//  AdvertisingAgency.swift
+//  topcoder
+// http://community.topcoder.com/stat?c=problem_statement&pm=7558
+//
+//  Created by Max on 05/06/15.
+//  Copyright (c) 2015 Max. All rights reserved.
+//
+
+import Foundation
+
+class AdvertisingAgency {
+    
+    class func numberOfRejections(requests : [Int]) -> Int {
+        
+        return 0;
+    }
+}
--- swift/topcoder/topcoderTests/topcoderTests.swift
+++ swift/topcoder/topcoderTests/topcoderTests.swift
@@ -8,28 +8,33 @@
 
 import Cocoa
 import XCTest
+import topcoder
 
 class topcoderTests: XCTestCase {
     
     override func setUp() {
         super.setUp()
-        // Put setup code here. This method is called before the invocation of each test method in the class.
     }
     
     override func tearDown() {
-        // Put teardown code here. This method is called after the invocation of each test method in the class.
         super.tearDown()
     }
     
-    func testExample() {
-        // This is an example of a functional test case.
-        XCTAssert(true, "Pass")
+    func testAdvertisingAgency() {
+        XCTAssertEqual(AdvertisingAgency.numberOfRejections([1,2,3]), 0)
+        XCTAssertEqual(AdvertisingAgency.numberOfRejections([1,1,1]), 2)
+        XCTAssertEqual(AdvertisingAgency.numberOfRejections([1,2,1,2]), 2)
+        XCTAssertEqual(AdvertisingAgency.numberOfRejections([
+            100, 100, 100, 100, 100, 100, 100, 100, 100, 100,
+            100, 100, 100, 100, 100, 100, 100, 100, 100, 100,
+            100, 100, 100, 100, 100, 100, 100, 100, 100, 100,
+            100, 100, 100, 100, 100, 100, 100, 100, 100, 100,
+            100, 100, 100, 100, 100, 100, 100, 100, 100, 100
+            ]), 49)
     }
     
     func testPerformanceExample() {
-        // This is an example of a performance test case.
         self.measureBlock() {
-            // Put the code you want to measure the time of here.
         }
     }
     
